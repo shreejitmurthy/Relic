544a7a28868aab564b2927991523c19233fa830a
Shreejit Murthy <botmorph17@gmail.com>
2024-10-06 13:12:03 +1030

    added extra SDL OpenGL attribute, for macOS only

--- Engine/src/Window.cpp
+++ Engine/src/Window.cpp
@@ -16,6 +16,7 @@ Window::Window(WindowArgs args) : _width(args.width), _height(args.height), _tit
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
 
     open = true;
 
