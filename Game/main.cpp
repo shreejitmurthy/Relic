26a90fed72333ec11afcb1e699828df103a34a54
Shreejit Murthy <botmorph17@gmail.com>
2024-10-11 12:21:38 +1030

    working dear imgui debug window

--- Game/main.cpp
+++ Game/main.cpp
@@ -9,13 +9,17 @@
 #include <cmath>
 #include <iostream>
 
-const int screenWidth = 800, screenHeight = 600;
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_sdl3.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
+const int screenWidth = 1280, screenHeight = 720;
 
 class Player : Actor {
 public:
     Player(std::string path, TextureQuad quad = { 0, 0, 0, 0 }, const glm::vec2& position = { 0, 0 }, const glm::vec2& scale = { 1, 1 })
     {
-        _texture = new Texture(path);
+        texture = new Texture(path);
         this->position = position;
         this->scale = scale;
         _tintTime = 0.0f;
@@ -32,7 +36,9 @@ public:
 
         if (kb->isDown(SDLK_LEFT)) {
             position.x -= 1;
-            std::cout << "Container x-pos: " << position.x << std::endl;
+//            std::cout << "Container x-pos: " << position.x << std::endl;
+        } else if (kb->isDown(SDLK_RIGHT)) {
+            position.x += 1;
         }
     }
 
@@ -41,13 +47,13 @@ public:
         if (quad.w == 0 || quad.h == 0) {
             drawQuad = {
                     0, 0,
-                    static_cast<float>(_texture->getWidth()),
-                    static_cast<float>(_texture->getHeight())
+                    static_cast<float>(texture->getWidth()),
+                    static_cast<float>(texture->getHeight())
             };
         } else {
             drawQuad = quad;
         }
-        _texture->draw((TextureDrawArgs){
+        texture->draw((TextureDrawArgs){
                 .position = position,
                 .quad = drawQuad,
                 .scale = scale,
@@ -61,7 +67,6 @@ public:
     TextureQuad quad;
     float moveSpeed;
 private:
-    Texture* _texture;
     glm::vec4 _tint;
     float _tintTime;
     float _tintUpdateSpeed;
@@ -73,6 +78,20 @@ int main() {
         .width = screenWidth,
         .height = screenHeight,
     });
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.IniFilename = nullptr;
+
+    ImGuiWindowFlags im_window_flags = 0;
+    im_window_flags |= ImGuiWindowFlags_NoResize;
+    im_window_flags |= ImGuiWindowFlags_NoMove;
+
+    ImGui_ImplSDL3_InitForOpenGL(window.window, window.ctx);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    ImGui::StyleColorsDark();
 
     Shader textureShader((ShaderArgs){
         .vertex_path = "../Game/shaders/texture.vert",
@@ -88,7 +107,6 @@ int main() {
     glm::vec2 scale = glm::vec2(1, 1);
 
     Player player("../Game/resources/container.png", {0, 0, 256, 256}, position, {1, 1});
-
 
     Texture knight("../Game/resources/knight.png");
 
@@ -108,8 +126,29 @@ int main() {
             window.open = false;
         }
 
+        ImGui_ImplSDL3_ProcessEvent(&window.event);
+
         player.update(window.kb, 0.f);
         cam.setPosition(player.position);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            // GUI Window
+            ImGui::Begin("Debug", nullptr, im_window_flags);
+
+            ImGui::SetWindowPos({5, 5});
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        ImGui::Render();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -120,24 +159,25 @@ int main() {
 
         knight.draw((TextureDrawArgs){
             .position = {200, 300},
-            .scale = {3, 3},
+            .scale = {5, 5},
+            .rotation = 45.f,
             .shader = textureShader,
         });
 
         cam.detach();
-
 
         fontShader.use();
         fontShader.set_mat4("projection", fontProjection);
 
         font.print((FontPrintArgs){
             .text = "Hello, World!",
-            .position = position,
+            .position = {500, 200},
             .colour = {0, 1, 0},
             .scale = 1,
             .shader = fontShader,
         });
 
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         window.refresh();
     }
 
