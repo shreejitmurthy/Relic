f1afa9ce90628925facd32fe57e63f40db910e30
Shreejit Murthy <botmorph17@gmail.com>
2024-10-26 22:12:38 +1030

    added proper use of animations and spritesheets

--- Game/Include/Player.hpp
+++ Game/Include/Player.hpp
@@ -5,24 +5,33 @@
 #pragma once
 
 #include "Actor.hpp"
+#include <unordered_map>
+#include <memory>
+
+#include "Spritesheet.h"
+
+enum PLAYER_STATES {
+    IDLE,
+    RUN,
+};
 
 class Player : Actor {
 public:
-    Player(std::string path,
-           TextureQuad quad = { 0, 0, 0, 0 },
-           const glm::vec2& position = { 0, 0 },
+    Player(const glm::vec2& position = { 0, 0 },
            const glm::vec2& scale = { 1, 1 }
     );
+    ~Player() override = default;
 
     void update(Keyboard* kb, float deltaTime) override;
     void render(Shader shader) override;
 
     glm::vec2 position;
+
+    std::shared_ptr<Spritesheet> sheet;
+    std::unordered_map<PLAYER_STATES, std::shared_ptr<Animation>> animations;
+    std::shared_ptr<Animation> currentAnimation;
 private:
     glm::vec2 scale;
-    TextureQuad quad;
     float moveSpeed;
     glm::vec4 tint;
-    float tintTime;
-    float tintUpdateSpeed;
 };
--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -2,47 +2,37 @@
 // Created by Shreejit Murthy on 14/10/2024.
 //
 
-#include "../Include/Player.hpp"
+#include "Player.hpp"
+#include <iostream>
 
-Player::Player(std::string path, TextureQuad quad, const glm::vec2 &position, const glm::vec2 &scale) {
-    texture = new Texture(path);
+#include <map>
+#include <string_view>
+
+Player::Player(const glm::vec2 &position, const glm::vec2 &scale) {
     this->position = position;
     this->scale = scale;
-    tintTime = 0.0f;
-    tintUpdateSpeed = 0.05f;
     tint = {1, 1, 1, 1};
-    this->quad = quad;
+
+    sheet = std::make_shared<Spritesheet>("../Game/Resources/V1/player_V1.png", 48, 48);
+    animations[IDLE] = std::make_shared<Animation>(sheet->newAnimation({1, 1}, {1, 9}, 0.1));
+    currentAnimation = animations[IDLE];
 }
 
 void Player::update(Keyboard *kb, float deltaTime) {
-    // update based on keyboard input
-    tintTime += tintUpdateSpeed;
-    tint.a = (sin(tintTime) + 1.0f) / 2.0f;
-
     if (kb->isDown(SDLK_LEFT)) {
         position.x -= 1;
-//            std::cout << "Container x-pos: " << position.x << std::endl;
     } else if (kb->isDown(SDLK_RIGHT)) {
         position.x += 1;
     }
+
+    currentAnimation->update(deltaTime);
 }
 
 void Player::render(Shader shader) {
-    TextureQuad drawQuad;
-    if (quad.w == 0 || quad.h == 0) {
-        drawQuad = {
-                0, 0,
-                static_cast<float>(texture->getWidth()),
-                static_cast<float>(texture->getHeight())
-        };
-    } else {
-        drawQuad = quad;
-    }
-    texture->draw((TextureDrawArgs){
-            .position = position,
-//                .quad = drawQuad,
-            .scale = scale,
-            .tint = tint,
-            .shader = shader,
+    sheet->draw((SpritesheetDrawArgs){
+        .animation = *currentAnimation,
+        .position = position,
+        .tint = tint,
+        .shader = shader
     });
 }
