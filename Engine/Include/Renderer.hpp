878d8a318587ed8d457f4a81b68c9c537c844507
Shreejit Murthy <botmorph17@gmail.com>
2024-12-10 01:56:37 +1030

    texture AND camera support

--- Engine/Include/Renderer.hpp
+++ Engine/Include/Renderer.hpp
@@ -4,15 +4,17 @@
 #include "Texture.hpp"
 #include "Font.hpp"
 #include "Camera2D.hpp"
+#include "Window.hpp"
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Renderer {
 public:
     Renderer();
     ~Renderer();
 
-    void init();
+    void init(Window& window);
     void beginScene(Camera2D &camera);
     void endScene();
 
@@ -23,11 +25,12 @@ public:
             glm::vec2 scale = {1.f, 1.f}, 
             float rotation = 0.f,
             glm::vec4 tint = {0.f, 0.f, 0.f, 0.f});
-            
+
     void renderFont(Font& font, FontPrintArgs& args);
 private:
     Shader textureShader;
     Shader fontShader;
     glm::mat4 projection;
     glm::mat4 view;
+    int width, height;
 };
--- Engine/Source/Renderer.cpp
+++ Engine/Source/Renderer.cpp
@@ -3,22 +3,35 @@
 //
 
 #include "Renderer.hpp"
+#include <glm/glm.hpp>
 
 Renderer::Renderer() {}
 Renderer::~Renderer() {}
 
-void Renderer::init() {
+void Renderer::init(Window& window) {
     textureShader.load("Engine/Shaders/texture.vert", "Engine/Shaders/texture.frag");
     fontShader.load("Engine/Shaders/font.vert", "Engine/Shaders/font.frag");
+    width = window.width;
+    height = window.height;
+
 }
 
-void Renderer::beginScene(Camera2D &camera) {
+void Renderer::beginScene(Camera2D& camera) {
     projection = camera.getProjection();
     view = camera.getView();
 
     textureShader.use();
     textureShader.set_mat4("view", camera.getView());
     textureShader.set_mat4("projection", camera.getProjection());
+
+    fontShader.use();
+    fontShader.set_mat4("projection", camera.getProjection());
+}
+
+void Renderer::endScene() {
+    textureShader.set_mat4("view", glm::mat4(1.f));
+    textureShader.set_mat4("projection", glm::ortho(0.f, static_cast<float>(width), static_cast<float>(height), 0.f, -1.f, 1.f));
+    fontShader.set_mat4("projection", glm::ortho( 0.f, static_cast<float>(width), 0.f, static_cast<float>(height), -1.f, 1.f));
 }
 
 void Renderer::renderTexture(Texture& texture, glm::vec2 position, TextureQuad quad, glm::vec2 scale, float rotation, glm::vec4 tint) {
