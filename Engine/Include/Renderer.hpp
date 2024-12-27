272821bd1f196592d3e5f4080f4bd83cb913ad36
Shreejit Murthy <botmorph17@gmail.com>
2024-12-27 19:23:52 +0800

    added rendering with descriptors c style for no reason

--- Engine/Include/Renderer.hpp
+++ Engine/Include/Renderer.hpp
@@ -16,6 +16,23 @@
 #include <unordered_map>
 #include <string>
 
+typedef struct texture_render_desc {
+    Texture texture;
+    glm::vec2 position;
+    TextureQuad quad;
+    glm::vec2 scale;
+    float rotation;
+    glm::vec4 tint;
+} texture_render_desc;
+
+typedef struct font_render_desc {
+    Font font;
+    std::string text;
+    glm::vec2 position;
+    glm::vec4 colour;
+    float scale;
+} font_render_desc;
+
 class Renderer {
 public:
     void init(Window& window);
@@ -29,8 +46,10 @@ public:
             glm::vec2 scale = {1.f, 1.f}, 
             float rotation = 0.f,
             glm::vec4 tint = {0.f, 0.f, 0.f, 0.f});
-
     void render(Font& font, std::string text, glm::vec2 position, glm::vec4 colour = glm::vec4(1.f), float scale = 1.f);
+    void render(font_render_desc desc);
+    void render(texture_render_desc desc);
+
 
 private:
     Shader textureShader;
--- Engine/Source/Renderer.cpp
+++ Engine/Source/Renderer.cpp
@@ -7,17 +7,17 @@
 
 #include <log/log.h>
 
+#include <functional>
+
 /*
  * Renderer Goal:
- * - Automatic texture packing for optimisation, this must be done in renderer
- *   and not in the Texture class. We ask to load a variadic amount of textures, then pack it.
- *   Use stb_rect_pack or create our own texture packer that can be ran and checked if already exists
- *   at initialisation.
- *   Then ensure that the renderer knows the locations of each texture, as each texture has it's own
- *   vertices. So the texture packed atlas will have its own vertices to subdivide the textures, then
- *   each texture will have it's own vertices for quads.
- *   Use **only** for single textures, a spritesheet is already an atlas.
-*/
+ * Pack images into one file using Python script. 
+ * MAYBE: The renderer knows the position and dimensions of each image, and is the one to pack the images.
+ * Then submit an object to the renderer, where an image ID and TextureQuad are passed in.
+ * The renderer will then render the imageID at a specified position.
+ */
+
+// TODO: Implement texture packing
 
 void Renderer::init(Window& window) {
     textureShader.load("Engine/Shaders/texture.vert", "Engine/Shaders/texture.frag");
@@ -55,6 +55,24 @@ void Renderer::render(Texture& texture, glm::vec2 position, TextureQuad quad, gl
     texture.draw(position, textureShader, quad, scale, rotation, tint);
 }
 
+void Renderer::render(texture_render_desc desc) {
+    desc.quad = desc.quad.w ? desc.quad : TextureQuad{
+        0, 0, 
+        static_cast<float>(desc.texture.getWidth()), 
+        static_cast<float>(desc.texture.getHeight())
+    };
+    desc.scale = (desc.scale.x || desc.scale.y) ? desc.scale : glm::vec2{1.f, 1.f};
+    desc.rotation = desc.rotation ? desc.rotation : 0.f;
+    desc.tint = is_zero_colour(desc.tint) ? glm::vec4{1.f, 1.f, 1.f, 1.f} : desc.tint;
+    render(desc.texture, desc.position, desc.quad, desc.scale, desc.rotation, desc.tint);
+}
+
 void Renderer::render(Font& font, std::string text, glm::vec2 position, glm::vec4 colour, float scale) {
     font.print(text, position, fontShader, colour, scale);
+}
+
+void Renderer::render(font_render_desc desc) {
+    desc.colour = is_zero_colour(desc.colour) ? glm::vec4{1.f, 1.f, 1.f, 1.f} : desc.colour;
+    desc.scale = desc.scale ? desc.scale : 1.f;
+    render(desc.font, desc.text, desc.position, desc.colour, desc.scale);
 }
